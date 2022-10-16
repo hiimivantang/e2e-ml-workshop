083b055cb91fcae36616dfb164e2c1b3a5271e20
hiimivantang <hiimivantang>
2022-10-16 08:27:09 +0000

    Remove manual table creation since we have a DLT pipeline.

---
+++ 04-deploy serving endpoint.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC 1. Click models on the left sidebar
+# MAGIC 2. Search for your model, i.e. `model_fraud` and click it.
+# MAGIC 3. Click `Use model for inference`
+# MAGIC 4. Select `Real-time` and click `Enable Serverless Real-Time Inference`
